922225d6d22a51152cfc481f3472acb5aebc4aca
iamyunusali <aliyunus291@gmail.com>
2022-08-01 12:26:35 +0300

    fix linters

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
     static: './dist',
   },
   output: {
-    filename: 'index.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
   optimization: {
