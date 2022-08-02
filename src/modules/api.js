7412701147b5199de0001dae48f873dd3566c5d9
sameh <sameh080081@gmail.com>
2022-08-03 00:30:50 +0400

    fix linters errors

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,6 +1,5 @@
-import displayData from './display.js';
+import displayData, { data } from './display.js';
 import displayPopup from './popup.js';
-import { data } from './display';
 
 const fetchAPI = async () => {
   await fetch('https://yts.mx/api/v2/list_movies.json?genre=documentary&limit=12&sort_by=download_count')
@@ -13,7 +12,6 @@ const fetchAPI = async () => {
       displayPopup(data, e.target.id);
     });
   });
-
 };
 
 export default fetchAPI;
